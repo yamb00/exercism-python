d588ae5e7ce3c8c0a8046801522ff3e85727d668
yamb00 <micha.johannes.lerch@gmail.com>
2023-06-09 13:31:52 +0000

    add docstring leap_year

--- exercises/practice/leap/leap.py
+++ exercises/practice/leap/leap.py
@@ -1,2 +1,14 @@
 def leap_year(year):
+    """
+    Check if a given year is a leap year.
+
+    A leap year in the Gregorian calendar occurs on every year that is evenly divisible by 4,
+    except every year that is evenly divisible by 100 unless the year is also evenly divisible by 400.
+
+    Args:
+        year (int): The year to check.
+
+    Returns:
+        bool: True if the year is a leap year, False otherwise.
+    """
     pass
