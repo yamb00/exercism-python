055e66351210e3903309cdd58c66c485b14c11be
yamb00 <micha.johannes.lerch@gmail.com>
2023-06-09 13:37:48 +0000

    add docstring to practice resistor-color

--- exercises/practice/resistor-color/resistor_color.py
+++ exercises/practice/resistor-color/resistor_color.py
@@ -1,6 +1,24 @@
 def color_code(color):
+    """
+    Look up the numerical value associated with a particular color band on a resistor.
+
+    Args:
+        color (str): The color of the band on the resistor.
+
+    Returns:
+        int: The numerical value associated with the color band.
+
+    Raises:
+        ValueError: If the given color is not a valid resistor color.
+    """
     pass
 
 
 def colors():
+    """
+    List the different band colors available for resistors.
+
+    Returns:
+        list: A list of strings representing the available band colors.
+    """
     pass
